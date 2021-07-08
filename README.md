3e63173d2c40ca0562d723a57222eaa0b3eaaf4e
JC-LL <jc.lelann@gmail.com>
2021-07-08 16:38:21 +0200

    first commit : first animation commands elaborated

---
+++ README.md
@@ -0,0 +1,31 @@
+# Animating SVG using Ruby through _Websockets_  
+
+## Presentation
+A set of small experiments to drive SVG animation _via websockets_.
+I struggled too much on trying to animate scenes using various GUI librairies.
+My conclusion is that there's nothing better than SVG.
+
+Here, I use a simple Ruby websocket server on one side and a simple browser (my case Firefox) that displays the SVG. The server is here to send drawing commands of my own, to a Javascript client that applies the commands to the SVG elements. We can also imagine that the client can send status or user events back to the server.
+
+## Contents description
+
+* **Test 1** : where I started playing with the idea. Various commands are prototyped.
+* **Test 2** : animating a finite-state machine (TO COME)
+* **Test 3** : organizing something more robust (TO COME)
+
+## How to install ?
+
+Simply clone the github projet project.
+
+## How to reproduce ?
+
+```[bash]
+  cd test_1
+  cd server
+  ruby app.rb #start the server
+```
+Then open test_1/browser/index.html in your browser. An animation should be displayed.
+
+
+## Questions ?
+If you have suggestions, plase contact me **jean-christophe.le_lann** at ensta-bretagne dot fr.
---
+++ test_1/server/app.rb
@@ -0,0 +1,51 @@
+require_relative 'websocket_server'
+require "json"
+
+include Math
+
+server = WebSocketServer.new
+
+COLORS=['red','blue','green','gold','chartreuse','chocolate']
+DX,DY,R=100,100,50
+loop do
+  Thread.new(server.accept) do |connection|
+    puts "Connected"
+    while true
+      message = connection.recv
+      if message
+        puts "Received from js : #{message}"
+      end
+
+      for i in 0..360
+        deg=10*i
+        color=COLORS.sample
+        x,y=DX+R*sin(deg*PI/180),DY+R*cos(deg*PI/180)
+        transform="translate(#{DX+x},#{DY+y}) rotate(#{deg})"
+        font="Verdana"
+        font_size=20+10*sin(i)**2
+        stroke_width=4
+        rb_msg=[
+          {cmd: "move"     , id: "circle_1" , args: [x,y] },
+          {cmd: "chgcolor" , id: "circle_1" , args: [color]   },
+          {cmd: "translate", id: "circle_2" , args: [DX+x,y] },
+          {cmd: "translate", id: "rect_1"   , args: [DX+x,100] },
+          {cmd: "translate", id: "rect_2"   , args: [3*DY,DX+x] },
+          {cmd: "rotate"   , id: "rect_3"   , args: [deg] },
+          {cmd: "rotate"   , id: "square_1" , args: [-deg] },
+          {cmd: "rotate"   , id: "square_2" , args: [2*deg] },
+          {cmd: "transform", id: "rect_4"   , args: [transform] },
+          {cmd: "rotate"   , id: "arrow"    , args: [deg] },
+          {cmd: "text"     , id:"text_1"    , args: [2*y,2*x,font,font_size,stroke_width,color]},
+          {cmd: "text"     , id:"text_2"    , args: [2*x,2*y,font,font_size,stroke_width,COLORS.sample]},
+          {cmd: "rotate"   , id:"text_3"    , args: [3*deg]}
+        ]
+        (3..11).each do |c|
+          rb_msg << {cmd: "dmove"    , id: "circle_#{c}", args: [rand(-5..5),rand(-5..5)]  }
+          rb_msg << {cmd: "chgcolor" , id: "circle_#{c}", args: [color]   }
+        end
+        connection.send(JSON.generate(rb_msg));
+        sleep 0.1
+      end
+    end
+  end
+end
